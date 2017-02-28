e18052bb1beb3050ec51bbd7f08031902082e8ba
Ching Wen <10210604@mail.ntua.edu.tw>
2017-02-28 12:35:23 +0800

    change works system style

---
+++ javascripts/main.js
@@ -0,0 +1,14 @@
+angular.module('gungho-girl', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/state1")
+        $stateProvider
+        .state('state1', {
+          url: "/state1",
+          templateUrl: "templates/state1.html"
+        })
+        .state('works-pink', {
+          url: "/works-pink",
+          templateUrl: "templates/works-pink.html"
+        })        
+
+    })
